f57c10363fbd577e7b8f76dbc760e75d67e54072
Vedant-Parekh <vedantbparekh@gmail.com>
2022-02-27 02:32:12 +0530

    Update custom_bash_commands.sh

--- custom_bash_commands.sh
+++ custom_bash_commands.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+
+# Takes a single number from 0 to 100 as input and sets the maximum battery charge to that value
+charge-limit(){
+if [[ $# -eq 0 ]]
+ then
+    	echo "No arguments supplied"
+else
+	sudo cp ~/Customizations/Charge_Limit/sample-battery-charge-limit.service /etc/systemd/system/battery-charge-threshold.service
+	sudo sed -i "s/CHARGE_STOP_THRESHOLD/$1/g" /etc/systemd/system/battery-charge-threshold.service
+	sudo systemctl daemon-reload
+	sudo systemctl restart battery-charge-threshold.service
+fi	
+}
+
+# Creates multiple files and copies correct boilerplate in the files based on their extension
 create(){
     echo "Creating Files"
     while (( "$#" )); do
@@ -6,18 +22,23 @@ create(){
         read -a strarr <<< "$text"
         extension=${strarr[1]}
         touch "$text"
-        cp /home/vedant/ubuntu/customizations/Boilerplates/boilerplate."$extension" ./"$text" 2>/dev/null
+        cp ~/Customizations/Boilerplates/boilerplate."$extension" ./"$text" 2>/dev/null
         shift
     done
 }
 
+# For cses practice problems
 cses(){
-    cd /mnt/c/Users/vedan/Home/Hobbies/CP/CSES
+    cd ~/Hobbies/CP/CSES
     code .
 }
 
+# Creates a folder for a contest and adds files with boilerplates
+# Use flag -c to specify number of files to be created
+# Use flag -n to specify name of folder
+# Use flag -f to overwrite folder with specified name
 CP(){
-    cd /mnt/c/Users/vedan/Home/Hobbies/CP/codeforces
+    cd ~/Hobbies/CP/codeforces
     name_of_contest="contest"
     number_of_questions=5
     force=0
@@ -54,5 +75,7 @@ CP(){
 
 #Aliases
 alias open='xdg-open'
-alias sql='sudo service mysql restart; sudo mysql'
 alias python='python3'
+
+# clearing journal files more than two weeks old
+alias clear-journal='journalctl --vacuum-time=3d'
